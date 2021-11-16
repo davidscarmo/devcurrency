cae00ed71db6d447dcc9f7a071e6121cd2ba4305
David Carmo <davidsc2812@gmail.com>
2021-11-16 05:09:01 -0300

    primeira versão de conversão basica

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,15 @@
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { FormEvent, useState } from "react";
 import { api } from "../services/api";
 import styles from "../styles/Home.module.css";
-
 
 interface Currency {
   currencyCode: string;
   name: string;
-  buy: string;
+  buy: number;
+  symbol: string;
   variation: number;
 }
 interface CurrenciesProps {
@@ -16,13 +17,29 @@ interface CurrenciesProps {
 }
 
 export default function Home(props: CurrenciesProps) {
-  console.log(props.currencies[0].buy);
+  const [valueToConvert, setValueToConvert] = useState(null);
+  const [convertedValue, setConvertedValue] = useState(null);
+  const handleConvertCurrency = (event: FormEvent) => {
+    event.preventDefault();
+    const value = valueToConvert * props.currencies[0].buy;
+    setConvertedValue(value);
+  };
+
   return (
     <>
       <Head>
         <title>Dev Currency</title>
       </Head>
       <div>Hello there!</div>
+      <form onSubmit={handleConvertCurrency}>
+        <input
+          type="number"
+          value={valueToConvert > 0 ? valueToConvert : ''}
+          onChange={(event) => setValueToConvert(Number(event.target.value))}
+        />
+        <button type="submit">Converter</button>
+      </form>
+      {convertedValue}
     </>
   );
 }
@@ -31,16 +48,19 @@ export const getServerSideProps: GetServerSideProps = async () => {
   const response = await api.get("/");
   const currenciesData = response.data.results.currencies;
   const currenciesConvertedData = Object.entries(currenciesData)
+    .filter((currency) => currency[0] !== "source")
     .map((currency: any) => {
       return {
         currencyCode: currency[0] || null,
         name: currency[1].name || null,
         buy: currency[1].buy || null,
+        symbol: new Intl.NumberFormat("pt-BR", {
+          style: "currency",
+          currency: currency[0],
+        }).formatToParts()[0].value,
         variation: currency[1].variation || 0,
       };
-    })
-    .filter((currency) => currency.currencyCode !== "source");
-
+    });
   return {
     props: {
       currencies: currenciesConvertedData,
